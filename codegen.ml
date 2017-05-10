1f12cd5e938962e1577a0ebb8235e4be9fdc5d72
Beaudan <beaudan.brown@gmail.com>
2017-05-10 17:22:24 +1000

    Finished exprs (except arrays)

--- codegen.ml
+++ codegen.ml
@@ -13,33 +13,71 @@ let return () = Stack.pop scope;;
 
 let this_scope () = Stack.top scope;;
 
-let print_binop fmt (op, lt, rt) =
+let type_str fmt t =
+	match t with
+	| Int -> fprintf fmt "int";
+	| Float -> fprintf fmt "real";
+	| Bool -> fprintf fmt "bool";;
+
+let cooerce fmt (lt, rt, lreg, rreg) =
+	match lt, rt with
+	| Int, Float ->
+		fprintf fmt "@,int_to_real r%i, r%i"
+		lreg lreg;
+	| Float, Int ->
+		fprintf fmt "@,int_to_real r%i, r%i"
+		rreg rreg;
+	| _, _ -> ();;
+
+let binop_str fmt op =
+	match op with
+	| Op_add -> fprintf fmt "add_";
+	| Op_sub -> fprintf fmt "sub_";
+	| Op_mul -> fprintf fmt "mul_";
+	| Op_div -> fprintf fmt "div_";
+	| Op_eq -> fprintf fmt "cmp_eq_";
+	| Op_neq -> fprintf fmt "cmp_ne_";
+	| Op_lt -> fprintf fmt "cmp_lt_";
+	| Op_gt -> fprintf fmt "cmp_gt_";
+	| Op_gteq -> fprintf fmt "cmp_ge_";
+	| Op_lteq -> fprintf fmt "cmp_le_";
+	| Op_and -> fprintf fmt "and";
+	| Op_or -> fprintf fmt "or";;
+
+let unop_str fmt op =
+	match op with
+	| Op_not -> fprintf fmt "not";
+	| Op_minus -> ();;
+
+let print_binop fmt (id, op, { id = lid }, { id = rid }) =
 	let rreg = !reg in
 	decr reg;
 	let lreg = !reg in
+	let t = lookup_type id in
+	let lt = lookup_type lid in
+	let rt = lookup_type rid in
 	match op with
-	| Op_add -> (
-		match lt, rt with
-		| Int, Int -> fprintf fmt "@,add_int r%i, r%i, r%i" lreg lreg rreg;
-		| Int, Float ->
-			fprintf fmt "@,int_to_real r%i, r%i@,add_real r%i, r%i, r%i"
-			lreg lreg lreg lreg rreg
-		| Float, Int ->
-			fprintf fmt "@,int_to_real r%i, r%i@,add_real r%i, r%i, r%i"
-			rreg rreg lreg lreg rreg
-		| Float, Float -> fprintf fmt "@,add_real r%i, r%i, r%i" lreg lreg rreg;
-		| _, _ -> ())
-	| Op_sub -> ()
-	| Op_mul -> ()
-	| Op_div -> ()
-	| Op_eq -> ()
-	| Op_neq -> ()
-	| Op_lt -> ()
-	| Op_gt -> ()
-	| Op_gteq -> ()
-	| Op_lteq -> ()
-	| Op_or -> ()
-	| Op_and -> ()
+	| Op_or ->
+		fprintf fmt "@,or r%i, r%i, r%i"
+		lreg lreg rreg;
+	| Op_and ->
+		fprintf fmt "@,and r%i, r%i, r%i"
+		lreg lreg rreg;
+	| _ ->
+		fprintf fmt "%a@,%a%a r%i, r%i, r%i"
+		cooerce (lt, rt, lreg, rreg)
+		binop_str op
+		type_str t
+		lreg lreg rreg;;
+
+let print_unop fmt (op, { id = id }) =
+	match op with
+	| Op_not ->
+		fprintf fmt "@,not r%i, r%i" !reg !reg
+	| Op_minus ->
+		let t = lookup_type id in
+		fprintf fmt "@,%a_const r%i, -1@,mul_%a r%i, r%i, r%i"
+		type_str t (!reg + 1) type_str t !reg !reg (!reg + 1);;
 
 let rec generate_expr fmt expr =
 	match expr with
@@ -61,22 +99,23 @@ let rec generate_expr fmt expr =
 			incr reg;
 			fprintf fmt "@,load r%i, %i" !reg stack;
 		| _ -> print_string "Not implemented\n"; exit 0;)
-	| { expr = Ebinop (({id = lid} as lexpr), (op, _, _), ({id = rid} as rexpr))} ->
-		generate_expr fmt lexpr;
-		generate_expr fmt rexpr;
-		let lt = lookup_type lid in
-		let rt = lookup_type rid in
-		print_binop fmt (op, lt, rt);
-	| { expr = Eunop ((unop, _, _), expr); id = id } -> ();
-	| { expr = Earray (ident, exprs); id = id } -> ();;
+	| { expr = Ebinop (lexpr, (op, _, _), rexpr); id = id } ->
+		fprintf fmt "%a%a%a"
+		generate_expr lexpr
+		generate_expr rexpr
+		print_binop (id, op, lexpr, rexpr);
+	| { expr = Eunop ((op, _, _), expr) } ->
+		fprintf fmt "%a%a"
+		generate_expr expr
+		print_unop (op, expr);
+	| { expr = Earray (ident, exprs) } -> ();;
 
-let print_write fmt ({ expr = e; id = id } as expr) =
-	generate_expr fmt expr;
-	decr reg;
-	match lookup_type id with
-	| Int -> fprintf fmt "@,call_builtin print_int";
-	| Float -> fprintf fmt "@,call_builtin print_real";
-	| Bool -> fprintf fmt "@,call_builtin print_bool";;
+let print_write fmt ({ id = id } as expr) =
+	(match lookup_type id with
+	| Int -> fprintf fmt "%a@,call_builtin print_int" generate_expr expr;
+	| Float -> fprintf fmt "%a@,call_builtin print_real" generate_expr expr;
+	| Bool -> fprintf fmt "%a@,call_builtin print_bool" generate_expr expr;);
+	decr reg;;
 
 let rec generate_stmts fmt stmts =
 	match stmts with
@@ -161,4 +200,5 @@ let rec generate_procs fmt prog =
 
 let generate prog =
 	let fmt = Format.std_formatter in
-	fprintf fmt "@[<v>@[<v 4>    call proc_main@,halt@]@,%a@,@]" generate_procs prog;;
+	fprintf fmt "@[<v>@[<v 4>    call proc_main@,halt@]@,%a@,@]"
+	generate_procs prog;;
