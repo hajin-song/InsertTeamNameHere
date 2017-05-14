f691848ffade1eb5971ce8d8d055b5b42733b905
Ha Jin Song <hSong6230@gmail.com>
2017-05-14 17:00:27 +1000

    hajin/commenting

--- snick.ml
+++ snick.ml
@@ -27,12 +27,23 @@ let main () =
   (* Initialize lexing buffer *)
   let lexbuf = Lexing.from_channel infile in
   (* Call the parser *)
-  let prog = Snick_parse.program Snick_lex.token lexbuf in
-  match !mode with
-  | PrettyPrint ->
-    Snick_pprint.print_program Format.std_formatter prog 
-  | Compile -> 
-    Analyze.semantic_analysis prog;
-    Codegen.generate prog;;
-
+  try
+   let prog = Snick_parse.program Snick_lex.token lexbuf in
+   match !mode with
+   | PrettyPrint ->
+     Snick_pprint.print_program Format.std_formatter prog
+   | Compile ->
+     Analyze.semantic_analysis prog;
+     Codegen.generate prog;
+  with
+   | Snick_lex.Syntax_error msg ->
+    let (ln, col) = Snick_lex.get_lex_pos lexbuf in
+    Printf.fprintf stderr
+      "Lexer error: %s at line %i, column %i\n" msg ln col;
+    exit 1
+   | Parsing.Parse_error ->
+    let (ln, col) = Snick_lex.get_lex_pos lexbuf in
+    Printf.fprintf stderr
+      "Parse error at line %i, column %i\n" ln col;
+     exit 1
 let _ = main ()
